4a60eceaf07909d177fc9d7e963b845303b94f12
Hugo Leonardo de Oliveira Rocha <hugo.rocha@seniorsolution.com.br>
2017-12-15 17:32:29 -0200

    Initial commit for the runner project

---
+++ Runner/Config.xaml.cs
@@ -0,0 +1,175 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Runner
+{
+    /// <summary>
+    /// Interaction logic for Config.xaml
+    /// </summary>
+    public partial class Config : MetroWindow
+    {
+        public Config()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Normal;
+            this.Activate();
+            MessageBox.Show(Properties.Settings.Default.AcceptanceTests[0]);
+            Loader();
+        }
+
+        private void Loader()
+        {
+            LoadTests();
+            LoadImageFolders();
+            LoadLayout();
+        }
+
+        private void LoadTests()
+        {
+            if (Properties.Settings.Default.AcceptanceTests != null)
+            {
+                var tests = Properties.Settings.Default.AcceptanceTests;
+                listBoxTests.Items.Clear();
+
+                foreach (var item in tests)
+                {
+                    ListBoxItem listBoxItem = new ListBoxItem();
+                    listBoxItem.Content = item;
+                    listBoxTests.Items.Add(listBoxItem);
+                }
+            }
+        }
+
+        private void LoadImageFolders()
+        { }
+
+
+        private void LoadLayout()
+        { }
+
+        private async void AbrirDiretorioTeste(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+
+            dialog.InitialDirectory = "C:".TrimEnd(new char[] { '\\' });
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                var files = Directory.GetFiles(dialog.FileName, "*Cest*.php", SearchOption.TopDirectoryOnly);
+                List<string> fileList = files.OfType<string>().ToList();
+                var progressDialog = await this.ShowProgressAsync("Aguarde...", "Estou vasculhando os arquivos.");
+                progressDialog.SetIndeterminate();
+
+                this.Dispatcher.Invoke(() => ProcessarArquivos(fileList, progressDialog, dialog.FileName));
+            }
+        }
+
+        private async void ProcessarArquivos(List<string> files, ProgressDialogController dialog, string directory)
+        {
+            listBoxTests.Items.Clear();
+
+            foreach (var file in files)
+            {
+
+                TextReader textReader = File.OpenText(file);
+                if (!string.IsNullOrEmpty(textReader.ReadToEnd()))
+                {
+                    ListBoxItem listBoxItem = new ListBoxItem();
+                    listBoxItem.Content = file;
+                    listBoxTests.Items.Add(listBoxItem);
+                }
+                textReader.Close();
+            }
+            WriteToSettings(listBoxTests.Items, directory);
+            Dispatcher.Invoke(() => flyOutTests.IsOpen = true);
+            textBoxTestsDirectory.Text = directory;
+            await dialog.CloseAsync();
+        }
+
+        private void WriteToSettings(object listBoxTests, string directory)
+        {
+
+            System.Collections.Specialized.StringCollection acceptanceTests = new System.Collections.Specialized.StringCollection();
+
+            foreach (ListBoxItem item in listBoxTests as ItemCollection)
+            {
+                acceptanceTests.Add(item.Content.ToString());
+            }
+            Properties.Settings.Default.AcceptanceTests = acceptanceTests;
+            Properties.Settings.Default.AcceptanceTestsFolder = directory;
+            Properties.Settings.Default.Save();
+        }
+
+        private void LookupTests(object sender, RoutedEventArgs e)
+        {
+            flyOutTests.IsOpen = true;
+        }
+
+        private void RevalidarDiretorio(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxTestsDirectory.Text))
+            {
+                flyOutTests.IsOpen = true;
+                progressRingFlyout.IsActive = true;
+                listBoxTests.Items.Clear();
+                var files = Directory.GetFiles(textBoxTestsDirectory.Text, "*Cest*.php", SearchOption.TopDirectoryOnly);
+                List<string> fileList = files.OfType<string>().ToList();
+                foreach (var file in files)
+                {
+
+                    TextReader textReader = File.OpenText(file);
+                    if (!string.IsNullOrEmpty(textReader.ReadToEnd()))
+                    {
+                        ListBoxItem listBoxItem = new ListBoxItem();
+                        listBoxItem.Content = file;
+                        listBoxTests.Items.Add(listBoxItem);
+                    }
+                    textReader.Close();
+                }
+
+                progressRingFlyout.IsActive = false;
+            }
+            else
+            {
+                this.ShowMessageAsync("Erro", "Não foi informado um diretório!");
+            }
+        }
+
+        /// <summary>
+        /// Helper. Busca pelos tipos de objetos definidos no depObj.
+        /// Usado para buscar todos os objetos de um tipo.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="depObj"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Runner/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Runner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        const int WM_SYSCOMMAND = 0x0112;
+        const int SC_MOVE = 0xF010;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.SourceInitialized += ScreenBlocker;
+
+        }
+
+        /// <summary>
+        /// Handler to start up the screen blocker treatment.
+        /// </summary>
+        private void ScreenBlocker(object sender, EventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            HwndSource source = HwndSource.FromHwnd(helper.Handle);
+            source.AddHook(WndProc);
+        }
+
+        /// <summary>
+        /// Protect the screen from moving.
+        /// </summary>
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+
+            switch (msg)
+            {
+                case WM_SYSCOMMAND:
+                    int command = wParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE)
+                    {
+                        handled = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        /// <summary>
+        /// On Load event of the application.
+        /// </summary>
+        private void Loader(object sender, RoutedEventArgs e)
+        {
+            var desktopArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopArea.Right - this.Width;
+            this.Top = desktopArea.Bottom - this.Height;
+        }
+
+
+        /// <summary>
+        /// Open the configuration window.
+        /// </summary>
+        private void Configuration(object sender, RoutedEventArgs e)
+        {
+            bool isWindowOpen = false;
+
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window is Config)
+                {
+                    isWindowOpen = true;
+                    window.Activate();
+                }
+            }
+
+            if (!isWindowOpen)
+            {
+                Config configWindow = new Config();
+                configWindow.Owner = this;
+                configWindow.Show();
+            }
+        }
+
+        /// <summary>
+        /// Open the runner window.
+        /// </summary>
+        private void TestRunner(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
